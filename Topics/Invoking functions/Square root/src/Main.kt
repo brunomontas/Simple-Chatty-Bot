5b5da56359d5dd4906f9db983c6e2301266b8f15
brunomontas <brunomon@gmail.com>
2023-03-06 14:46:46 +0000

    first project

---
+++ Topics/Invoking functions/Square root/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // do not change this code
+    val number = readln().toDouble()
+    val squareRootOfNumber = Math.sqrt(number)
+    println(squareRootOfNumber)// write your code below
+
+}
---
+++ Topics/String templates/String and its Int/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    // write here
+    val number = readln().toInt()
+    print("The obtained value is $number and its Int representation after division on 2 is ${number / 2}")
+}
